f5b2e2030d155f929f6d699087dd6c38cbe38278
jamie <jamie.cheng@yuansuan.com>
2025-03-11 11:48:24 +0800

    ts 基础数据类型

---
+++ ts/test.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: jamie jamie.cheng@yuansuan.com
+ * @Date: 2025-03-11 11:13:44
+ * @LastEditors: jamie jamie.cheng@yuansuan.com
+ * @LastEditTime: 2025-03-11 11:29:02
+ * @FilePath: \cjmLearn\ts\test.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+var Color;
+(function (Color) {
+    Color["Red"] = "red";
+    Color["black"] = "black";
+    Color["Blue"] = "blue";
+})(Color || (Color = {}));
+var c = Color.Blue;
+console.log(c);
---
+++ ts/test.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: jamie jamie.cheng@yuansuan.com
+ * @Date: 2025-03-11 11:13:44
+ * @LastEditors: jamie jamie.cheng@yuansuan.com
+ * @LastEditTime: 2025-03-11 11:29:02
+ * @FilePath: \cjmLearn\ts\test.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+enum Color {
+  Red = "red",
+  black = "black",
+  Blue = "blue",
+}
+let c: Color = Color.Blue;
+console.log(c);
