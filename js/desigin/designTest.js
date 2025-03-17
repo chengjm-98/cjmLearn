c57786e8f7607dcd698b53a165513ad94df2457f
jamie <jamie.cheng@yuansuan.com>
2025-03-17 14:40:04 +0800

    单例实际应用场景

--- js/desigin/designTest.js
+++ js/desigin/designTest.js
@@ -1,10 +1,3 @@
-// class Singleton {
-//     static #instance: Singleton;
-//     private constructor() { }
-//     public static get instance(): Singleton {
-//         if (!Singleton.#instance) {
-//             Singleton.#instance = new Singleton();
-//         }
 var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
     if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
@@ -16,39 +9,28 @@ var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
     return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
-var _a, _MySingleton_instance;
-//         return Singleton.#instance;
-//     }
-//     public someBusinessLogic() {
-//         // ...
-//     }
-// }
-// function clientCode() {
-//     const s1 = Singleton.instance;
-//     const s2 = Singleton.instance;
-//     if (s1 === s2) {
-//         console.log(
-//             'Singleton works, both variables contain the same instance.'
-//         );
-//     } else {
-//         console.log('Singleton failed, variables contain different instances.');
-//     }
-// }
-// clientCode();
-class MySingleton {
+var _a, _configManager__instance;
+class configManager {
+    // private config: { [key: string]: any } = {};  //这种写法也可以
     constructor() {
-        //构造函数私有化
+        this.config = {}; //Record<string,any> 会生成一个对象，对象的key是string，value是any
     }
-    //一个公有的静态获取实例的方法
     static getInstance() {
-        if (!__classPrivateFieldGet(_a, _a, "f", _MySingleton_instance)) {
-            __classPrivateFieldSet(_a, _a, new _a(), "f", _MySingleton_instance);
+        if (!__classPrivateFieldGet(_a, _a, "f", _configManager__instance)) {
+            __classPrivateFieldSet(_a, _a, new _a(), "f", _configManager__instance);
         }
-        return __classPrivateFieldGet(_a, _a, "f", _MySingleton_instance);
+        return __classPrivateFieldGet(_a, _a, "f", _configManager__instance);
+    }
+    getConfig(key) {
+        return this.config[key];
+    }
+    setConfig(key, value) {
+        this.config[key] = value;
     }
 }
-_a = MySingleton;
-_MySingleton_instance = { value: void 0 }; //静态私有属性，用来存储 实例
-let a = MySingleton.getInstance();
-let b = MySingleton.getInstance();
-console.log("是否是单例模式：", a === b);
+_a = configManager;
+_configManager__instance = { value: void 0 };
+const config = configManager.getInstance();
+config.setConfig("name", "张三");
+config.setConfig("age", 18);
+console.log(config.getConfig("name"));
--- js/desigin/designTest.ts
+++ js/desigin/designTest.ts
@@ -1,47 +1,24 @@
-// class Singleton {
-//     static #instance: Singleton;
-//     private constructor() { }
-//     public static get instance(): Singleton {
-//         if (!Singleton.#instance) {
-//             Singleton.#instance = new Singleton();
-//         }
-
-//         return Singleton.#instance;
-//     }
-//     public someBusinessLogic() {
-//         // ...
-//     }
-// }
-// function clientCode() {
-//     const s1 = Singleton.instance;
-//     const s2 = Singleton.instance;
-
-//     if (s1 === s2) {
-//         console.log(
-//             'Singleton works, both variables contain the same instance.'
-//         );
-//     } else {
-//         console.log('Singleton failed, variables contain different instances.');
-//     }
-// }
-
-// clientCode();
-
-class MySingleton {
-  static #instance: MySingleton; //静态私有属性，用来存储 实例
-  private constructor() {
-    //构造函数私有化
-  }
-  //一个公有的静态获取实例的方法
-  public static getInstance(): MySingleton {
-    if (!MySingleton.#instance) {
-      MySingleton.#instance = new MySingleton();
+class configManager {
+  static #_instance: configManager;
+  private config: Record<string, any> = {}; //Record<string,any> 会生成一个对象，对象的key是string，value是any
+  // private config: { [key: string]: any } = {};  //这种写法也可以
+  private constructor() {}
+  public static getInstance(): configManager {
+    if (!configManager.#_instance) {
+      configManager.#_instance = new configManager();
     }
-    return MySingleton.#instance;
+    return configManager.#_instance;
+  }
+
+  public getConfig(key: string): any {
+    return this.config[key];
+  }
+  public setConfig(key: string, value: any): void {
+    this.config[key] = value;
   }
 }
 
-let a = MySingleton.getInstance();
-let b = MySingleton.getInstance();
-
-console.log("是否是单例模式：", a === b);
+const config = configManager.getInstance();
+config.setConfig("name", "张三");
+config.setConfig("age", 18);
+console.log(config.getConfig("name"));
