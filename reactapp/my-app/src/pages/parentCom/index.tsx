1ad2667fd7a72a4baf9c13b8032df0a680249d00
Chengjm <2316872140@qq.com>
2025-11-06 23:34:32 +0800

    [try]

--- reactapp/my-app/src/pages/parentCom/index.tsx
+++ reactapp/my-app/src/pages/parentCom/index.tsx
@@ -1,12 +1,21 @@
-import { useState, useRef } from "react";
-import ChildCom from "../childCom";
+import { useState, useRef, useEffect } from "react";
 import { Button } from "antd";
 export const ParentCom = () => {
-  const [data, setData] = useState("cjm");
   const [count, setCount] = useState(0);
+  const asyncFun = async()=>{
+   return 11;''
+  }
+  useEffect(()=>{
+    const myFun = async()=>{
+  const data = await asyncFun();
+   console.log('看一下async的返回值',data)
+    
+    }
+  myFun();
+   
+  },[])
   return (
     <>
-      <ChildCom name={data} />
       <Button onClick={() => setCount(count + 1)}>change</Button>
     </>
   );
